bda9be7f2d8e9d07d1b07b45ed22fd9dedf2e39d
Vladimir Zheltoukhov <ava.izet@gmail.com>
2019-12-28 12:59:29 +0300

    Terraform files

---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable project {
+  description = "Project ID"
+}
+variable region {
+  description = "Region"
+  # Значение по умолчанию
+  default = "europe-west1"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable disk_image {
+  description = "Disk image"
+}
+variable private_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable zone {
+  # Описание переменной
+  description = "Zone"
+}
+variable public_key {
+  # Описание переменной
+  description = "public_key"
+}
