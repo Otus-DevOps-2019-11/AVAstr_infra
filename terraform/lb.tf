01f788ca8b3dc9331dca5f917d9079744f0a5e54
Vladimir Zheltoukhov <ava.izet@gmail.com>
2019-12-28 19:35:06 +0300

    Update LB

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -1,15 +1,15 @@
 resource "google_compute_forwarding_rule" "lb-fw" {
   name                  = "reddit-app"
-  description           = "lb rule for reddit apps"
+  description           = "rule for reddit apps"
   port_range            = "9292"
-  target                = "${google_compute_target_pool.lb.self_link}"
+  target                = google_compute_target_pool.lb.self_link
   load_balancing_scheme = "EXTERNAL"
 }
 
 resource "google_compute_target_pool" "lb" {
   name          = "reddit-lb"
-  description   = "lb for reddit apps"
-  instances     = "${google_compute_instance.app.*.self_link}"
+  description   = "lb for eddit apps"
+  instances     = google_compute_instance.app.*.self_link
   health_checks = ["${google_compute_http_health_check.default.name}"]
 }
 
